495ec0e4877981e6c27618e6793f6bf782bab7c8
Diego Fernando Alayon <dfar.41@gmail.com>
2021-12-07 16:02:02 -0500

    creando notas mediante un formulario, y usando la accion de cambiar importancia en cada nota

--- src/index.js
+++ src/index.js
@@ -5,43 +5,72 @@ import { noteReducer } from './reducers/noteReducer';
 
 const store = createStore(noteReducer)
 
-store.dispatch({
-  type: '@notes/created',
-  payload: {
-    content: 'hola hola nota hona',
-    important: true,
-    id: 1
-  }
-})
-store.dispatch({
-  type: '@notes/created',
-  payload: {
-    content: 'tengo hambre 2',
-    important: false,
-    id: 2
-  }
-})
+// store.dispatch({
+//   type: '@notes/created',
+//   payload: {
+//     content: 'hola hola nota hona',
+//     important: true,
+//     id: 1
+//   }
+// })
+// store.dispatch({
+//   type: '@notes/created',
+//   payload: {
+//     content: 'tengo hambre 2',
+//     important: false,
+//     id: 2
+//   }
+// })
+const generateId = () => Math.floor(Math.random() * 9999999999) + 1
 
 const App = () => {
   const state = store.getState()
-
-  return (
-    <ul>
-      {
-        state.map((note) => {
-          return <li key={note.id}>
-            {note.content}
-            <strong>
-              {
-                note.important
-                  ? ' important'
-                  : ' not important'
-              }
-            </strong>
-          </li>
-        })
+  const addNote = (event) => {
+    event.preventDefault()
+    const {target} = event
+    const content = target.note.value
+    target.note.value=''
+    store.dispatch({
+      type:'@notes/created',
+      payload: {
+        content,
+        important: false,
+        id: generateId()
       }
-    </ul>
+    })
+  }
+  const toggleImportant = (id) => {
+    store.dispatch({
+      type: '@notes/toggle_important',
+      payload: {
+        id
+      }
+    })
+  }
+  return (
+    <div>
+      <form onSubmit={addNote}>
+        <input name='note' />
+        <button>add</button>
+      </form>
+      <ul>
+        {
+          state.map((note) => {
+            return <li key={note.id} onClick={() => toggleImportant(note.id)}>
+              {note.content}
+              <strong>
+                {
+                  note.important
+                    ? ' important'
+                    : ' not important'
+                }
+              </strong>
+            </li>
+          })
+        }
+      </ul>
+    </div>
+
   )
 }
 
