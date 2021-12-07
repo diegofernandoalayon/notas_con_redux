38d24fbdd6cab042c1ca6def0d6770e25c18f0c0
Diego Fernando Alayon <dfar.41@gmail.com>
2021-12-07 16:45:11 -0500

    separar el componente App del punto de entrada de la aplicacion

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import { createNote, toggleImportanceOf } from './reducers/noteReducer';
 
-function App() {
+const App = ({store}) => {
+  const state = store.getState()
+  const addNote = (event) => {
+    event.preventDefault()
+    const {target} = event
+    const content = target.note.value
+    target.note.value=''
+    store.dispatch(createNote(content))
+  }
+  const toggleImportant = (id) => {
+    store.dispatch(toggleImportanceOf(id))
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form onSubmit={addNote}>
+        <input name='note' />
+        <button>add</button>
+      </form>
+      <ul>
+        {
+          state.map((note) => {
+            return <li key={note.id} onClick={() => toggleImportant(note.id)}>
+              {note.content}
+              <strong>
+                {
+                  note.important
+                    ? ' important'
+                    : ' not important'
+                }
+              </strong>
+            </li>
+          })
+        }
+      </ul>
     </div>
-  );
-}
 
-export default App;
+  )
+}
+export default App
--- src/index.js
+++ src/index.js
@@ -1,54 +1,14 @@
 import React from 'react';
 import ReactDOM, { render } from 'react-dom';
 import { createStore } from 'redux'
-import { noteReducer, createNote, toggleImportanceOf } from './reducers/noteReducer';
-
+import { noteReducer } from './reducers/noteReducer';
+import App from './App';
 const store = createStore(noteReducer)
-
-
-const App = () => {
-  const state = store.getState()
-  const addNote = (event) => {
-    event.preventDefault()
-    const {target} = event
-    const content = target.note.value
-    target.note.value=''
-    store.dispatch(createNote(content))
-  }
-  const toggleImportant = (id) => {
-    store.dispatch(toggleImportanceOf(id))
-  }
-  return (
-    <div>
-      <form onSubmit={addNote}>
-        <input name='note' />
-        <button>add</button>
-      </form>
-      <ul>
-        {
-          state.map((note) => {
-            return <li key={note.id} onClick={() => toggleImportant(note.id)}>
-              {note.content}
-              <strong>
-                {
-                  note.important
-                    ? ' important'
-                    : ' not important'
-                }
-              </strong>
-            </li>
-          })
-        }
-      </ul>
-    </div>
-
-  )
-}
 
 const renderApp = () => {
   ReactDOM.render(
     <React.StrictMode>
-      <App />
+      <App store = {store} />
     </React.StrictMode>,
     document.getElementById('root')
   );
