dfc800772a0f011838ffb6cbd352f72c90992a79
Diego Fernando Alayon <dfar.41@gmail.com>
2021-12-09 14:35:30 -0500

    agregando estado inicial

--- src/reducers/noteReducer.js
+++ src/reducers/noteReducer.js
@@ -1,4 +1,17 @@
-export const noteReducer = (state = [], action) => {
+const initialState = [
+  {
+    content: 'bienvenidos a la clase',
+    important: true,
+    id: 1
+  },
+  {
+    content: 'gracias',
+    important: false,
+    id: 2
+  }
+]
+
+export const noteReducer = (state = initialState, action) => {
   if(action.type === '@notes/created') {
     return state.concat(action.payload)
   }
