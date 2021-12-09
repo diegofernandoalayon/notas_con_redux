d48edc0266bae479450096cca2c746c3b896ba04
Diego Fernando Alayon <dfar.41@gmail.com>
2021-12-09 15:08:28 -0500

    usando combineReducers para usar mas de un reducer a la hora de crear la Store

--- src/App.js
+++ src/App.js
@@ -2,9 +2,20 @@ import NewNote from './components/NewNote';
 import Notes from './components/Notes';
 
 const App = () => {
+  const filterSelected = value => {
+    console.log(value)
+  }
   return (
     <div>
       <NewNote />
+      <div>
+        all
+        <input type='radio' name='filter' onChange={() => filterSelected('ALL')} />
+        important
+        <input type='radio' name='filter' onChange={() => filterSelected('IMPORTANT')} />
+        not important
+        <input type='radio' name='filter' onChange={() => filterSelected('NOT_IMPORTANT')} />
+      </div>
       <Notes />
     </div>
 
--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM, { render } from 'react-dom';
-import { createStore } from 'redux'
+import { createStore, combineReducers } from 'redux'
 import { Provider } from 'react-redux';
 import { noteReducer } from './reducers/noteReducer';
 import App from './App';
-const store = createStore(noteReducer)
+const reducer = combineReducers({
+  notes: noteReducer,
+  filter: filterReducer
+})
+const store = createStore(reducer)
 
 
 ReactDOM.render(
